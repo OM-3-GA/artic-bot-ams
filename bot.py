e0a014c25b4c92405c4e479fbbd7a1e28c8ee312
OM-3-GA <55928680+OM-3-GA@users.noreply.github.com>
2019-10-01 15:51:33 -0400

    Update bot.py

--- bot.py
+++ bot.py
@@ -42,4 +42,4 @@ for filename in os.listdir('./cogs'):
     if filename.endswith('.py'):
         client.load_extension(f"cogs.{filename[:-3]}")
 
-client.run(token)
+client.run('secretAccessKey: process.env.TOKEN')
